8187466c689595fde56ee7e986eb371215d78a55
radumojic <radumojic@elrond.com>
2020-11-18 21:18:08 +0200

    Fixed some typos in footer/icons Renamed docs from /technology to match the ones required in sidebars.json removed the "Wallet" entry from sidebars json because that doc file does not exist, should be reintroduced once the doc is ready

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -43,7 +43,7 @@ export default function CustomFooter() {
 						 focusable="false" 
 						 data-prefix="fas" 
 						 data-icon="heart" 
-						 class="svg-inline--fa fa-heart fa-w-16 text-danger mx-1"
+						 className="svg-inline--fa fa-heart fa-w-16 text-danger mx-1"
 						 role="img" 
 						 xmlns="http://www.w3.org/2000/svg" 
 						 viewBox="0 0 512 512"
@@ -58,7 +58,7 @@ export default function CustomFooter() {
 			{/* App switcher icon and dropdown had to be placed here so we can have the functionality on all pages, since we don't have the possibility to edit the navigation menu*/}
 			<div id="app-switcher-dropdown">
 				<div className="switcher-icon" type="button">
-					<svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-labeledby="Menu">
+					<svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-labelledby="Menu">
 						<path d="M149.333 56v80c0 13.255-10.745 24-24 24H24c-13.255 0-24-10.745-24-24V56c0-13.255 10.745-24 24-24h101.333c13.255 0 24 10.745 24 24zm181.334 240v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.256 0 24.001-10.745 24.001-24zm32-240v80c0 13.255 10.745 24 24 24H488c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24zm-32 80V56c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.256 0 24.001-10.745 24.001-24zm-205.334 56H24c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24zM0 376v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 24zm386.667-56H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zm0 160H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zM181.333 376v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24z"></path>
 					</svg>
 				</div>
