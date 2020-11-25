aaf4d1dc95d0d5711a5529ee41ab99dc7c5b8b2d
radumojic <radumojic@elrond.com>
2020-11-25 18:20:26 +0200

    small text & layout changes

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -59,7 +59,7 @@ export default function CustomFooter() {
 			<div id="app-links">
 				<a href="https://github.com/ElrondNetwork" target="_blank" rel="noopener noreferrer" className="app-menu-entry d-flex align-items-center">
 					<svg className="app-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-						<path id="Path" class="st0" fillRule="evenodd" clipRule="evenodd" d="M12,0C5.4,0,0,5.4,0,12c0,5.3,3.4,9.8,8.2,11.4C8.8,23.5,9,23.1,9,22.8c0-0.3,0-1.2,0-2.2
+						<path id="Path" fillRule="evenodd" clipRule="evenodd" d="M12,0C5.4,0,0,5.4,0,12c0,5.3,3.4,9.8,8.2,11.4C8.8,23.5,9,23.1,9,22.8c0-0.3,0-1.2,0-2.2
 								c-3,0.6-3.8-0.7-4-1.4c-0.1-0.3-0.7-1.4-1.2-1.7c-0.4-0.2-1-0.8,0-0.8c0.9,0,1.6,0.9,1.8,1.2c1.1,1.8,2.8,1.3,3.5,1
 								c0.1-0.8,0.4-1.3,0.8-1.6c-2.7-0.3-5.5-1.3-5.5-5.9C4.4,10,4.8,9,5.6,8.1C5.5,7.8,5.1,6.6,5.7,5c0,0,1-0.3,3.3,1.2
 								c1-0.3,2-0.4,3-0.4s2,0.1,3,0.4C17.3,4.6,18.3,5,18.3,5c0.7,1.6,0.2,2.9,0.1,3.2c0.8,0.8,1.2,1.9,1.2,3.2c0,4.6-2.8,5.6-5.5,5.9
