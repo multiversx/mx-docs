c005c91c0bcb675fb9dd194f317e39ab966b9681
Dan Popa <popa.ioan.dan@gmail.com>
2021-02-10 11:25:25 +0200

    Adds growth.elrond.com link in app switcher

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -169,6 +169,14 @@ export default function CustomFooter() {
             <a href="/" className="dropdown-item active">
               Docs
             </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://growth.elrond.com/"
+              className="dropdown-item"
+            >
+              Growth
+            </a>
           </nav>
         </div>
       </div>
