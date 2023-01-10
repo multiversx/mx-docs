3150f3760bdd4ac90380e98ee95f73df82f66319
Radu Mojic <radu.mojic@elrond.com>
2023-01-10 19:46:56 +0200

    [+] remove theme switch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,8 +75,8 @@ const config = {
     ({
       colorMode: {
         defaultMode: "dark",
-        disableSwitch: false,
-        respectPrefersColorScheme: true,
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
       },
       navbar: {
         logo: {
