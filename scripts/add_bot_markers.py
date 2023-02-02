639c7ba73ef8bf03b13f78f1999e7a7fa68c01d7
schimih <schiopumihaialexandru@gmail.com>
2023-02-02 14:19:14 +0200

    update script to exclude context

--- scripts/add_bot_markers.py
+++ scripts/add_bot_markers.py
@@ -3,6 +3,7 @@ from typing import List
 
 ABSTRACT_MARKER = "[comment]: # (mx-abstract)"
 EXCLUDE_FILE_MARKER = "[comment]: # (mx-exclude-file)"
+EXCLUDE_CONTEXT_MARKER = "[comment]: # (mx-exclude-context)"
 SEPARATOR = "[comment]: # (mx-context-auto)"
 WEAK_CONTENT_THRESHOLD = 10
 DOCS_ROOT = Path(__file__).parent.parent / "docs"
@@ -55,6 +56,10 @@ def main():
                 output_lines.append(line)
                 continue
 
+            if line.strip() == EXCLUDE_CONTEXT_MARKER:
+                output_lines.append(line)
+                continue
+
             if looks_like_code_block(line):
                 state_in_code_block = not state_in_code_block
 
@@ -62,6 +67,9 @@ def main():
                 line) and ABSTRACT_MARKER not in [one_line_before, two_lines_before]
 
             if is_new_context:
+                if should_skip_context(one_line_before, two_lines_before):
+                    output_lines.append(line)
+                    continue
                 output_lines.append(SEPARATOR)
                 output_lines.append("")
                 output_lines.append(line)
@@ -90,6 +98,8 @@ def main():
 def should_skip_file(file_content: str) -> bool:
     return EXCLUDE_FILE_MARKER in file_content
 
+def should_skip_context(one_line_before: str,two_lines_before: str) -> bool:
+    return EXCLUDE_CONTEXT_MARKER in [one_line_before, two_lines_before]
 
 def looks_like_code_block(line: str):
     return line.startswith("```")
