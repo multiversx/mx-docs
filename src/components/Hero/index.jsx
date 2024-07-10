72c59b9c3f6e2733d6d948cef9746471aa103f8d
Radu Mojic <radu.mojic@multiversx.com>
2024-07-10 17:02:01 +0300

    added tailwind

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="flex w-full flex-col items-center justify-center gap-8 lg:flex-row md:gap-5 md:pt-24 lg:gap-32 xl:gap-48">
+      <h2 className="mb-4 font-bold">Test Tailwind</h2>
+    </section>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+    container: false,
+  },
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["./src/**/*.{jsx,tsx,html}"],
+  plugins: [],
+};
