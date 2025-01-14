e608c2135d9a692c8a2ec8ba4142364740eae378
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-01-14 14:34:31 +0200

    Removed some info and added page to sidebars.js
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -590,6 +590,7 @@ const sidebars = {
               "sovereign/ethereum-l2",
               "sovereign/bitcoin-l2",
               "sovereign/solana-l2",
+              "sovereign/cross-chain-execution"
             ],
           },
           "sovereign/validators",
