f7d20ec9859a554c3dd64e91764e6bfc86b98d2d
Andrei Marinica <andrei.marinica@multiversx.com>
2026-01-15 20:34:26 +0200

    rust-tutorial-ci.sh fix

--- testing/rust-tutorial-ci.sh
+++ testing/rust-tutorial-ci.sh
@@ -4,8 +4,10 @@
 ## The tests are also taken from the tutorial.
 ## Tests are only run on the rust backend.
 
-cd extract-tutorial-code || exit 1
+SCRIPT_DIR="$(dirname "$0")"
+
+cd "$SCRIPT_DIR/extract-tutorial-code" || exit 1
 cargo run || exit 1
 
-cd ../crowdfunding || exit 1
+cd "$SCRIPT_DIR/crowdfunding" || exit 1
 cargo test || exit 1
